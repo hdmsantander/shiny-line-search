cda3dd9ce938640f441dcccfe3e06d62c82f224e
Roberto Fabián Escobar Santander <hdmsantander@gmail.com>
2019-11-10 23:38:16 +0000

    Actualizará la aplicación para su versión inicial.

--- app.R
+++ app.R
@@ -12,6 +12,7 @@
 
 # Declarando el uso de las librerías que usaremos.
 library(shiny)
+library(DT)
 library(plotly)
 
 ## Para evaluar el gradiente de la función declarada vamos a usar la 
@@ -48,6 +49,7 @@ if(production == FALSE) {
 #  my_csv <- read.csv(paste0(home_path,"file_name.csv"))
 ##########################################################################################
 
+options(digits=7)
 
 ###############
 ## Funciones ##
@@ -67,11 +69,13 @@ funcionNorma <- function(x) {
 ## - puntoInicial: El punto desde donde se parte para buscar el mínimo.
 ## - valorEpsilon: El nivel de precisión que se desea para el resultado obtenido.
 ## - numeroMaximoIteraciones: El número máximo de iteraciones antes de que se detenga el programa
-## 	 si no ha encontrado el mínimo.
+## 	 	si no ha encontrado el mínimo.
 ## - funcionAMin: La función que se va a minimizar.
-## 
+## - numResultadosAImprimir: El número de primeros resultados que se colocan en el data frame que contiene la
+## 		tabla de resultados,en caso de que no se proporcione, el valor por defecto es numeroMaximoIteraciones.
+##
 ## La función devuelve un data frame que contiene la tabla de resultados.
-busquedaDeLinea <- function(puntoInicial,valorEpsilon,numeroMaximoIteraciones,funcionAMin) {
+busquedaDeLinea <- function(puntoInicial,valorEpsilon,numeroMaximoIteraciones,funcionAMin,numResultadosAImprimir = numeroMaximoIteraciones) {
    
    ## Declarando el data frame vacío que vamos a usar posteriormente para
    ## guardar los datos generados.
@@ -79,8 +83,9 @@ busquedaDeLinea <- function(puntoInicial,valorEpsilon,numeroMaximoIteraciones,fu
       k=integer(),
       xk_1=double(),
       xk_2=double(),
-      Valorf=double(),
-      Normaf=double(),
+      ValorFPunto=double(),
+      NormaFPunto=double(),
+      Lambda=double(),
       sk_1=double(),
       sk_2=double(),
       stringsAsFactors=FALSE)
@@ -96,7 +101,8 @@ busquedaDeLinea <- function(puntoInicial,valorEpsilon,numeroMaximoIteraciones,fu
       
       if(valorNorma < valorEpsilon){
          normaMayorQueEpsilon <- FALSE
-         cat("Se alcanzó la precisión deseada, el valor óptimo está en: ", puntoActual ," Terminando.")
+         cat("Se alcanzó la precisión deseada, el valor óptimo está en: ", puntoActual ," Terminando.\n")
+         tabla[nrow(tabla)+1,] <- c(k,puntoActual[1],puntoActual[2],valorFuncion,valorNorma,valorMinLambda$par,vectorDireccion[1],vectorDireccion[2])
       }
       else {
          # Declaramos la función auxiliar que opera con lambda y se basa en la funcionAMin
@@ -112,9 +118,9 @@ busquedaDeLinea <- function(puntoInicial,valorEpsilon,numeroMaximoIteraciones,fu
          valorFuncion <- funcionAMin(puntoActual)
          valorNorma <- funcionNorma(grad(funcionAMin,puntoActual))
          
-         # Dado que estamos iterando sobre miles, guardamos resultados cada 100 iteraciones.
-         if (k%%100 == 0) {
-            tabla[nrow(tabla)+1,] <- c(k,puntoActual[1],puntoActual[2],valorFuncion,valorNorma,vectorDireccion[1],vectorDireccion[2])
+         # Guardamos en la tabla solo los primeros numResultadosAImprimir que se le pasó a la función como argumento
+         if (k<numResultadosAImprimir) {
+            tabla[nrow(tabla)+1,] <- c(k,puntoActual[1],puntoActual[2],valorFuncion,valorNorma,valorMinLambda$par,vectorDireccion[1],vectorDireccion[2])
          }
          
          # Preparación para el siguiente ciclo.
@@ -125,9 +131,16 @@ busquedaDeLinea <- function(puntoInicial,valorEpsilon,numeroMaximoIteraciones,fu
    return (tabla)
 }
 
-################################################
-## Declarando valores para generar el gráfico ##
-################################################
+## Declarando una función que hace el manejo de los eventos relacionados
+## con clicks en el contorno para seleccionar puntos.
+xy_point <- function(e) {
+   if(is.null(e)) return(c(1,2))
+   c(e$x,e$y)
+}
+
+############################################################
+## Declarando valores y funciones para generar el gráfico ##
+############################################################
 
 ## Tamaño de la caja sobre la cual generamos
 boxsize <- 5
@@ -144,34 +157,78 @@ expanded <- expand.grid(boundx, boundy)
 
 ## Generando los valores de z
 zet <- apply(X=expanded,1,funcionA)
+zet <- matrix(zet,byrow = TRUE,ncol = pointno)
+
+## Generando los contornos para la representación 2D de la función
+contorno <- plot_ly(x = boundx, y = boundy, z = zet,source = "contorno") %>% 
+   add_trace(autocontour=TRUE,contours = list(coloring="heatmap"),
+             line = list(color = "#ffffff"),type = "contour",
+             colorbar = list(thickness=10,len = 1,tickfont = list(size = 8))) %>% 
+   layout(title = list(text="Contornos de superficie", font = list(size=12)), paper_bgcolor= "#E8E8E8", plot_bgcolor= "#E8E8E8",autosize = TRUE,yaxis = list(scaleanchor = "x", scaleratio = 1,tickfont = list(size = 8)),
+          margin = list(l =50,r = 50, t = 50 , b = 50 ,pad = 0),
+          xaxis = list(tickfont = list(size = 8),clickmode = "event")) %>%
+   config(displayModeBar = F)
+
+## Generando la superficie
+superficie <- plot_ly(x=boundx,y=boundy,z = zet,type = "surface",height = 600, width = 600,colorbar=list(thickness=13,len=1,tickfont=list(size=10)))%>% add_trace(
+   contours = list(
+      z = list(
+         show=TRUE,
+         usecolormap=TRUE,
+         highlightcolor="#ff0000",
+         name="Valores",
+         project=list(z=TRUE)
+      )
+   ),showscale = FALSE
+)%>%
+   colorbar(title = "Valor de la\n función") %>% layout(autosize = TRUE,yaxis = list(scaleanchor = "x", scaleratio = 1,tickfont = list(size = 8)),
+                                                        margin = list(l =10,r = 10, t = 10 , b = 10 ,pad = 0),
+                                                        xaxis = list(tickfont = list(size = 8)),
+                                                        zaxis = list(tickfont = list(size = 8)),
+                                                        scene = list(
+                                                           camera=list(
+                                                              eye = list(x=0, y=-1.8, z=1),center = list(x =0,y=0,z=0)
+                                                           ),aspectratio = list(x=1,y=1,z=0.8)
+                                                        ))
+
+
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    titlePanel("Búsquedas de línea"),
    
    sidebarLayout(
-      sidebarPanel(sliderInput("numResultados", h3("Número de os"),
-                               min = 10, max = 1000, value = 10),
+      sidebarPanel(style="background-color:#E8E8E8",
+                   sliderInput("numIteraciones", h3("Número de iteraciones"),
+                               min = 10, max = 1000, value = 20),
                    numericInput("valorEpsilon", 
-                                h3("Valor de precisión deseado"), 
-                                value = 0.01),
-                   textInput("puntoInicial", 
-                                h3("Dos valores separados por comas donde se desea iniciar"), 
-                                value ="1,2"),
-                   sliderInput("numResultados", h3("Número de resultados deseados"),
-                               min = 10, max = 1000, value = 10)
+                                h3("Valor de precisión deseada"), 
+                                value = 0.01, step = 0.01),
+                   sliderInput("numResultados", h3("Número de resultados a mostrar en la tabla"),
+                               min = 0, max = 50 , value = 10),
+                   br(),
+                   h3("Selección de punto inicial",align="center"),
+                   div(plotlyOutput(outputId = "plotContorno",width = "auto",height = "300px"),align="center"),
+                   br(),
+                   div (actionButton("botonComenzarBusqueda", "Comenzar Búsqueda"),align="center")
+                   
       ),
       
       
       mainPanel(
          div(img(src="http://www.izt.uam.mx/wp-content/uploads/2019/01/logo-web-oficial2b.png"),style="text-align: center;"),
          h1("Fundamentos y modelos de optimización: Búsqueda de línea"),
-         p("El presente trabajo presenta los resultados de implementar la técnica de optimización de búsqueda en línea para la función:"),
-         withMathJax("$$x_{1}^{4}-2x_{2}x_{1}^{2}+x_{2}^{2}+x_{1}^{2}-2x_{1}+5$$"),
-         p("Los resultados se generan colocando los valores iniciales en las opciones del menú y ejecutando el programa. Esto genera 
-           resultados que se guardan en una hoja de cálculo que se descarga, asimismo se imprimen las últimas 10 iteraciones."),
-         div(plotlyOutput("plot"),align="center"),
-         p("Representación gráfica de la superficie generada por la función y sus valores en el intervalo:")
+         p("El presente trabajo presenta los resultados de implementar la técnica de optimización búsqueda de línea para la función:"),
+         withMathJax("$$ x_{1}^{4}-2x_{2}x_{1}^{2}+x_{2}^{2}+x_{1}^{2}-2x_{1}+5$$"),
+         p("Los resultados se generan seleccionando los valores iniciales en las opciones del menú, seleccionando en el plano 2D con un click el punto inicial que se desea usar para el método y dando click en el botón \"Comenzar Búsqueda\" Esto genera 
+           resultados que se pueden ver en la tabla inferior. Al generar los puntos se pueden observar en la gráfica a continuación, unidos por una línea que representa el camino de la aproximación."),
+         br(),
+         div(plotlyOutput("plotSuperficie"),align="center"),
+         div(style="height:200px;"),
+         p("Representación gráfica de la superficie generada por la función y sus valores en el intervalo:"),
+         h2("Resultados:"),
+         DT::dataTableOutput("tablaResultados")
       )
    )
    
@@ -180,19 +237,50 @@ ui <- fluidPage(
 # Definimos la lógica del servidor para hacer el algoritmo y el plot de la gráfica
 server <- function(input, output,session) {
    
-   output$plot <- renderPlotly({
-      plot_ly(x=boundx,y=boundy,z = matrix(zet,byrow = TRUE,ncol = pointno),type = "surface",height = 500, width = 500)%>% add_trace(
-         contours = list(
-            z = list(
-               show=TRUE,
-               usecolormap=TRUE,
-               highlightcolor="#ff0000",
-               project=list(z=TRUE)
-            )
-         ),showscale = FALSE
-      )%>%
-         colorbar(title = "Valor de la\n función")
-      })
+   options(warn = -1)
+   
+   resultados <- NULL
+   
+   numIteraciones <- reactive({input$numIteraciones})
+   valorEpsilon <- reactive({input$valorEpsilon})
+   numResultados <- reactive({input$numResultados})
+   point <- c(1,2)
+   
+   output$plotContorno <- renderPlotly({
+      event <- event_data("plotly_click",source = "contorno")
+      if(is.null(event)==FALSE){
+         point <<- xy_point(event)
+         
+      }else{
+         point <<- c(1,2)
+      }
+      
+      contorno %>% 
+         add_trace(x=point[1], y=point[2], type="scatter", mode="markers", name="Punto inicial",marker=list(color="#f50505"))
+   })
+   
+   output$plotSuperficie <- renderPlotly({
+      numIteraciones <- isolate(numIteraciones())
+      numResultados <- isolate(numResultados())
+      valorEpsilon <- isolate(valorEpsilon())
+      point <- isolate(point)
+      input$botonComenzarBusqueda
+      resultados <<- busquedaDeLinea(puntoInicial = point,valorEpsilon = valorEpsilon, numeroMaximoIteraciones = numIteraciones,funcionAMin = funcionA, numResultadosAImprimir=numResultados)
+      route <- resultados[,c(2,3,4,5)]
+      superficie %>% 
+         add_trace(x=route[,1],y=route[,2],z=route[,3],showlegend=FALSE,
+                   type = "scatter3d",mode = "lines+markers",
+                   marker = list(width = 15,color = route[,4],colorscale = list(c(0,"#34c0eb"),c(route[1,4],"#f02626"))),
+                   line = list(width = 2,color = "#000000" ))
+   })
+   
+   output$tablaResultados = DT::renderDataTable({
+      input$botonComenzarBusqueda
+      
+      format(resultados,10)
+   })
+   
+   
 }
 
 # Run the application 
